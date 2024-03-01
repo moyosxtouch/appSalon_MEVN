95eb587a0c6a2435e9e9fff1d360e19af7d01a99
moyo <moyosxtouch@gmail.com>
2024-03-01 12:58:36 -0600

    modificacion para evitar registro duplicados en citas

--- frontend/src/stores/Services.js
+++ frontend/src/stores/Services.js
@@ -2,13 +2,17 @@ import ServicesAPI from "@/api/ServicesAPI";
 import { defineStore } from "pinia";
 import { ref, onMounted } from "vue";
 export const useServicesStore = defineStore("services", () => {
+  const services = ref([]);
   onMounted(async () => {
     try {
       const { data } = await ServicesAPI.all();
-      console.log(data);
+
+      services.value = data;
     } catch (error) {
       console.log(error);
     }
   });
-  return {};
+  return {
+    services,
+  };
 });
--- frontend/src/views/appointments/ServicesView.vue
+++ frontend/src/views/appointments/ServicesView.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { useServicesStore } from "@/stores/Services";
-const services = useServicesStore();
+import ServiceItem from "@/components/ServiceItem.vue";
+const store = useServicesStore();
 </script>
 
 <template>
-  <div>
-    <h1>Desde Servicios</h1>
+  <h2 class="text-4xl font-extrabold text-white mt-10">Servicios</h2>
+
+  <p class="text-white text-lg mt-5">Elige al menos un servicio para tu cita</p>
+
+  <div class="grid grid-cols-2 gap-5 mt-5">
+    <ServiceItem
+      v-for="service in store.services"
+      :key="service._id"
+      :service="service"
+    />
   </div>
 </template>
